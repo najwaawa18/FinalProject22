49d06acc411d3918f3fa383bc429a60f61541fc9
MuNash <nash.collage.life@gmail.com>
2025-09-24 13:53:04 +0700

    Nashru 1

--- Test.java
+++ Test.java
@@ -2,6 +2,8 @@ public class Test {
     
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
+
+        System.out.println("Hi, Nashru udah join di kolaborasi ini");
         
     }
 }
